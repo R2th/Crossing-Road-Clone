574e90d4c7ac9a978338937f61a54db1b94c4e0d
R2th <haupro186@gmail.com>
2022-01-08 00:35:08 +0700

    Obstracles render

--- Game/Test_Game/Obstacles.h
+++ Game/Test_Game/Obstacles.h
@@ -5,23 +5,50 @@
 #include "Utils.h"
 using namespace std;
 
-class Obstacles : public GameObject
-{
+class Obstacles : public GameObject {
 private:
 	int speed = 0, tick = 0, bgColor, fgColor;
-
 protected:
 	int autoX = 1, autoY = 0;
-
 public:
 	Obstacles() : GameObject(0, 0, 0, 0, "unknownObstacles") {}
-	Obstacles(int x, int y, int _speed, int _bgColor, int _fgColor, string bufferKey, GraphicsController *&graphic) : GameObject(x, y, graphic->getBuffer(bufferKey)[0].size(), graphic->getBuffer(bufferKey).size(), bufferKey),
-																													  speed(_speed),
-																													  bgColor(_bgColor),
-																													  fgColor(_fgColor)
-	{
+	Obstacles(int x, int y, int _speed, int _bgColor, int _fgColor, string bufferKey, GraphicsController*& graphic) : 
+		GameObject(x, y, graphic->getBuffer(bufferKey)[0].size(), graphic->getBuffer(bufferKey).size(), bufferKey), 
+		speed(_speed),
+		bgColor(_bgColor),
+		fgColor(_fgColor)
+	{}
+	void render(GraphicsController*& graphic, int offset) {
+		//clearOldPos(graphic);
+		graphic->setBufferObject(graphic->getBuffer(bufferKey), this->x, this->y + offset, bgColor, fgColor);
+	}
+	void render(GraphicsController*& graphic, int offset, int bg) {
+		//clearOldPos(graphic);
+		graphic->setBufferObject(graphic->getBuffer(bufferKey), this->x, this->y + offset, bg, fgColor);
+	}
+	void render(GraphicsController*& graphic, int offset, int bg, int ch) {
+		//clearOldPos(graphic);
+		graphic->setBufferObject(graphic->getBuffer(bufferKey), this->x, this->y + offset, bg, ch);
+	}
+	void clearOldPos(GraphicsController*& graphic) {
+		GameObject::clearOldPos(graphic, bgColor, fgColor);
+	}
+
+	void setPos(int x, int y) {
+		this->x = x;
+		this->oldX = x;
+		this->y = y;
+		this->oldY = y;
+	}
+
+	bool isOutOfBound() {
+		return x + MAX_ENEMY_WIDTH < 1 || x > screenWidth;
+	}
+
+	int getTick() {
+		return tick;
 	}
 };
 
-// fell free to declare more class using the above template
-// feel*
+//fell free to declare more class using the above template
+//feel*
