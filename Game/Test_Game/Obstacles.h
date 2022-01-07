feb73d2b5ed78b5abe12cff611ede797fb1395ea
R2th <haupro186@gmail.com>
2022-01-08 00:35:22 +0700

    Obstracles moving

--- Game/Test_Game/Obstacles.h
+++ Game/Test_Game/Obstacles.h
@@ -18,6 +18,28 @@ public:
 		bgColor(_bgColor),
 		fgColor(_fgColor)
 	{}
+
+	void move(int x, int y)
+	{
+		tick++;
+
+		// Check if enough ticks yet
+		if (tick < speed)
+			return; //Nah, not enough
+		
+		tick %= speed;
+		this->oldX = this->x;
+		this->oldY = this->y;
+		this->x += x;
+		this->y += y;
+
+		if (isOutOfBound())
+			resetPos(x > 0);
+	}
+	virtual void resetPos(bool left = true) {
+		this->x = left? -MAX_ENEMY_WIDTH + 1 : screenWidth;
+	}
+
 	void render(GraphicsController*& graphic, int offset) {
 		//clearOldPos(graphic);
 		graphic->setBufferObject(graphic->getBuffer(bufferKey), this->x, this->y + offset, bgColor, fgColor);
