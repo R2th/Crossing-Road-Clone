7d65b2faaf74fcfc9b3a9f46e6e19310f75f0b3a
R2th <haupro186@gmail.com>
2022-01-08 00:30:54 +0700

    player moving

--- Game/Test_Game/Player.h
+++ Game/Test_Game/Player.h
@@ -4,14 +4,29 @@
 #include "GameObject.h"
 using namespace std;
 
-class Player : public GameObject {
+class Player : public GameObject
+{
 protected:
 public:
 	Player() : GameObject(0, 0, 0, 0, "player") {}
-	Player(GraphicsController*& graphic) :
-		GameObject(0, 0, graphic->getBuffer("player")[0].size(), graphic->getBuffer("player").size(), "player") {}
-	Player(int x, int y, GraphicsController*& graphic) : 
-		GameObject(x, y, graphic->getBuffer("player")[0].size(), graphic->getBuffer("player").size(), "player") {}
-
+	Player(GraphicsController *&graphic) : GameObject(0, 0, graphic->getBuffer("player")[0].size(), graphic->getBuffer("player").size(), "player") {}
+	Player(int x, int y, GraphicsController *&graphic) : GameObject(x, y, graphic->getBuffer("player")[0].size(), graphic->getBuffer("player").size(), "player") {}
 	
+	void update()
+	{
+		this->oldX = this->x;
+		this->oldY = this->y;
+	}
+
+	void move(int x, int y)
+	{
+		this->x += 2 * x;
+		this->y += y;
+	}
+
+	void setPos(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+	}
 };
