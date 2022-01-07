55d89a4ab018879137485e98a53f5c3930156a4a
R2th <haupro186@gmail.com>
2022-01-08 00:31:31 +0700

    render player

--- Game/Test_Game/Player.h
+++ Game/Test_Game/Player.h
@@ -11,7 +11,6 @@ public:
 	Player() : GameObject(0, 0, 0, 0, "player") {}
 	Player(GraphicsController *&graphic) : GameObject(0, 0, graphic->getBuffer("player")[0].size(), graphic->getBuffer("player").size(), "player") {}
 	Player(int x, int y, GraphicsController *&graphic) : GameObject(x, y, graphic->getBuffer("player")[0].size(), graphic->getBuffer("player").size(), "player") {}
-	
 	void update()
 	{
 		this->oldX = this->x;
@@ -22,6 +21,24 @@ public:
 	{
 		this->x += 2 * x;
 		this->y += y;
+	}
+
+	void render(GraphicsController *&graphic, int offset)
+	{
+		// clearOldPos(graphic);
+		graphic->setBufferWhite(graphic->getBuffer(bufferKey), oldX, oldY + offset, BG, 1);
+		graphic->setBufferWhite(graphic->getBuffer(bufferKey), x, y + offset, BG, 7);
+	}
+	void render(GraphicsController *&graphic, int offset, int bg, int ch)
+	{
+		// clearOldPos(graphic);
+		graphic->setBufferWhite(graphic->getBuffer(bufferKey), oldX, oldY + offset, bg, blueLight);
+		graphic->setBufferWhite(graphic->getBuffer(bufferKey), x, y + offset, bg, ch);
+	}
+
+	void clearOldPos(GraphicsController *&graphic)
+	{
+		GameObject::clearOldPos(graphic, BG, 7);
 	}
 
 	void setPos(int x, int y)
