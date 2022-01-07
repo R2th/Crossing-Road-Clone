6fd810c3a5d7e6a33a2961f5cd455c155175527d
R2th <haupro186@gmail.com>
2022-01-08 00:26:18 +0700

    coord and moving for gameObject

--- Game/Test_Game/GameObject.h
+++ Game/Test_Game/GameObject.h
@@ -15,17 +15,17 @@ public:
 	GameObject() : oldX(0), oldY(0), x(0), y(0), w(0), h(0), Items() {}
 	GameObject(int x, int y, int w, int h, string bufferKey) : Items(bufferKey), oldX(x), oldY(y), x(x), y(y), w(w), h(h) {}
 
-	// coord getPos() {
-	// 	return { x, y };
-	// }
-	// coord getOldPos() {
-	// 	return { oldX, oldY };
-	// }
-	// BOUNDINGBOX getBoundingBox() {
-	// 	return { x,y,w,h };
-	// }
+	coord getPos() {
+		return { x, y };
+	}
+	coord getOldPos() {
+		return { oldX, oldY };
+	}
+	BOUNDINGBOX getBoundingBox() {
+		return { x,y,w,h };
+	}
 
-	// virtual void move(int x, int y) = 0;
+	virtual void move(int x, int y) = 0;
 
 	// void render(GraphicsController*& graphic, int bgColor, int fgColor) {
 	// 	graphic->setBufferObject(graphic->getBuffer(bufferKey), x, y, bgColor, fgColor);
